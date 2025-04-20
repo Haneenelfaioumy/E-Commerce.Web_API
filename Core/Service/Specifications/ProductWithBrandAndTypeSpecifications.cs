2208bb09472072f46808b7af0cbc501c7b1454e2
Haneenelfaioumy <Haneenelfaioumy2001@gmail.com>
2025-04-20 06:43:27 +0200

    Part 07 Search Specification

--- Core/Service/Specifications/ProductWithBrandAndTypeSpecifications.cs
+++ Core/Service/Specifications/ProductWithBrandAndTypeSpecifications.cs
@@ -15,10 +15,14 @@ namespace Service.Specifications
         public ProductWithBrandAndTypeSpecifications(ProductQueryParams queryParams) 
             : base(P => (!queryParams.BrandId.HasValue || P.BrandId == queryParams.BrandId) 
                       && 
-                        (!queryParams.TypeId.HasValue || P.TypeId == queryParams.TypeId))
+                        (!queryParams.TypeId.HasValue || P.TypeId == queryParams.TypeId)
+                      &&
+                        (string.IsNullOrWhiteSpace(queryParams.SearchValue) || 
+                         P.Name.ToLower().Contains(queryParams.SearchValue.ToLower())))
         {
             AddInclude(P => P.ProductBrand);
             AddInclude(P => P.ProductType);
+
             switch (queryParams.SortingOption)
             {
                 case ProductSortingOptions.NameAsc:
