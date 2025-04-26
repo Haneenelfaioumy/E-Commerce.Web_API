7bb8d6b8c9e24a6deb274d19e734893df139ea17
Haneenelfaioumy <Haneenelfaioumy2001@gmail.com>
2025-04-26 04:23:21 +0300

    Part 09 Basket Module - Entities

--- Core/Service/MappingProfiles/PictureUrlResolver.cs
+++ Core/Service/MappingProfiles/PictureUrlResolver.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
-using DomainLayer.Models;
 using Shared.DataTransferObjects;
 using Microsoft.Extensions.Configuration;
+using DomainLayer.Models.ProductModule;
 
 namespace Service.MappingProfiles
 {
