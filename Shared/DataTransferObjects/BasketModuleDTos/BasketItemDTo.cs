147bab82ff3f46b9e63195b94aa851a99ad682bc
Haneenelfaioumy <Haneenelfaioumy2001@gmail.com>
2025-04-26 12:31:36 +0300

    Part 12 Basket Module - Service

---
+++ Shared/DataTransferObjects/BasketModuleDTos/BasketItemDTo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DataTransferObjects.BasketModuleDTos
+{
+    public class BasketItemDTo
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = default!;
+        public string PictureUrl { get; set; } = default!;
+
+        [Range(1 , int.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Range(1 , 100)]
+        public int Quantity { get; set; }
+    }
+}
